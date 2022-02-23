311772212b670698417a7a29051b6badc34cd663
VictorCov <victor.covarrubias.uaa@gmail.com>
2022-02-22 22:29:31 -0600

    Sleuth config

---
+++ admin-service/src/test/java/academy/digitallab/store/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.digitallab.store.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
